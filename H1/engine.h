6479efc4627c3d635da1a58e42bb72d86afe0149
MihaiStefanoaia <mihaistefanoaia1102@gmail.com>
2023-10-10 14:41:47 +0300

    added some comments to the engine

--- H1/engine.h
+++ H1/engine.h
@@ -6,15 +6,25 @@
 #define H1_ENGINE_H
 #include "funclib.h"
 
+class genome{
+public:
+    fixedpt* chromosomes; // using raw pointer as it allows for some alignment, and the size is managed by the engine
+    explicit genome(int size){
+        chromosomes = new fixedpt[size];
+    }
+};
+
 class engine {
 public:
-    static fixedpt** population;
-    static fixedpt** pop_buffer;
-    static uint32_t  pop_size;
-    static uint32_t  dimensions;
-    static float_t   selection_pressure;
-    static std::function<fixedpt(fixedpt*, int)> func;
-    static uint32_t  generations;
+    static genome*  population;   // the main population on which the work is being done
+    static genome*  pop_buffer;   // buffer for the new generations
+    static uint32_t pop_size;     // amount of entities
+    static uint32_t dimensions;   // dimensions to optimize - also number of chromosomes
+    static uint32_t generations;  // max amount of times to run the algorithm
+    static float_t  threshold;    // the stopping point
+    static float_t  selection_pressure;
+    static std::function<fixedpt(fixedpt*, uint32_t)> optimize; // function to optimize for
+    static std::function<float_t(fixedpt)> fitness;             // fitness function
 };
 
 
