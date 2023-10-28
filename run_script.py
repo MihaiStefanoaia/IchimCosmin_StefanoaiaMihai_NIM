f4dc22a53f5c99abd5986b9ae521c11170aa6b6d
MihaiStefanoaia <mihaistefanoaia1102@gmail.com>
2023-10-28 23:11:40 +0300

    final touches to run script and cmake for windows

--- run_script.py
+++ run_script.py
@@ -30,14 +30,17 @@ if __name__ == '__main__':
             for p in [100, 500, 1000]:
                 for cp in [45, 55, 65]:
                     for sp in [40, 50, 60]:
-                        outfile = open(f'results/f_{f}_d_{d}_p_{p}_cp_{cp}_sp_{sp}.txt', 'w')
+                        for hcf in [0, 1, 5]:
+                            for hcs in ['none', 'first', 'best']:
+                                filename = f'results/f_{f}_d_{d}_p_{p}_cp_{cp}_sp_{sp}_hcf_{hcf}_hcs_{hcs}'
+                                outfile = open(filename + '.txt', 'w')
 
-                        subprocesses += [subprocess.Popen(
-                            ['./genetic-engine', '-d', str(d), '-t', str(tolerances[f][d]), '-f', f, '-p', str(p), '-m', '0.000625',
-                             '-g', '1500', '-c', '2', '-fq', '10', '-cp', str(cp), '-sp', str(sp), '--log_file',
-                             f'results/f_{f}_d_{d}_p_{p}_cp_{cp}_sp_{sp}.json'],
-                            stdout=outfile)]
-                        print(f'started a run with function {f} at {d} dimensions')
+                                subprocesses += [subprocess.Popen(
+                                    ['./genetic-engine', '-d', str(d), '-t', str(tolerances[f][d]), '-f', f, '-p', str(p), '-m', '0.000625',
+                                     '-g', '1000', '-c', '2', '-fq', '10', '-cp', str(cp), '-sp', str(sp), '-hc', hcs, '-hcf', str(hcf), '--log_file',
+                                     (filename + '.json')],
+                                    stdout=outfile)]
+                                print(filename)
 
     for p in subprocesses:
         p.wait()
