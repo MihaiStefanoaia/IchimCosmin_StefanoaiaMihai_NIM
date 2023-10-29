e657b51634e6d188387f61e74b93d7ae17951a13
MihaiStefanoaia <mihaistefanoaia1102@gmail.com>
2023-10-29 13:15:45 +0200

    screept

--- run_script.py
+++ run_script.py
@@ -25,31 +25,33 @@ if __name__ == '__main__':
             30: -29
         }
     }
-    for f in ['rosenbrock', 'michalewicz']:
+    for f in ['rastrigin','griewangk', 'rosenbrock', 'michalewicz']:
         for d in [2, 10, 30]:
             for p in [100, 500, 1000]:
                 for hcs in ['none', 'first', 'best']:
+                    subprocesses = {}
                     for hcf in [0, 1, 5]:
-                        subprocesses = {}
-                        for i in range(5):
+                        for i in range(1):
                             ran_any = False
-                            for cp in [45, 55, 65]:
-                                for sp in [40, 50, 60]:
-                                    filename = f'results/f_{f}_d_{d}_p_{p}_hcs_{hcs}_hcf_{hcf}_cp_{cp}_sp_{sp}_{i}'
-                                    if os.path.isfile(filename + '.json'):
-                                        continue
-                                    outfile = open(filename + '.txt', 'w')
+                            for m in [0.001, 0.002, 0.00625, 0.01]:
+                                cp = 65
+                                sp = 50
+                                filename = f'aaaa/f_{f}_d_{d}_p_{p}_hcs_{hcs}_hcf_{hcf}_m_{m}_{i}'
+                                if os.path.isfile(filename + '.json'):
+                                    print(f'{filename} skipped')
+                                    continue
+                                outfile = open(filename + '.txt', 'w')
 
-                                    subprocesses[filename] = subprocess.Popen(
-                                        ['./genetic-engine', '-d', str(d), '-t', str(tolerances[f][d]), '-f', f, '-p', str(p), '-m', '0.000625',
-                                         '-g', '1000', '-c', '2', '-fq', '10', '-cp', str(cp), '-sp', str(sp), '-hc', hcs, '-hcf', str(hcf), '--log_file',
-                                         (filename + '.json')],
-                                        stdout=outfile)
-                                    print(filename)
-                                    ran_any = True
+                                subprocesses[filename] = subprocess.Popen(
+                                    ['./genetic-engine', '-d', str(d), '-t', str(tolerances[f][d]), '-f', f, '-p', str(p), '-m', '0.000625',
+                                        '-g', '1000', '-c', '2', '-fq', '10', '-cp', str(cp), '-sp', str(sp), '-hc', hcs, '-hcf', str(hcf), '--log_file',
+                                        (filename + '.json')],
+                                    stdout=outfile)
+                                print(filename)
+                                ran_any = True
                             if ran_any:
                                 time.sleep(2.0)
 
-                        for fn, pr in subprocesses.items():
-                            pr.wait()
-                            print(f'{fn} done')
+                    for fn, pr in subprocesses.items():
+                        pr.wait()
+                        print(f'{fn} done')
